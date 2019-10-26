a1768df4cffef3cdbd69c90e6e756c6703afb3db
Dany Pop <yjoedany@gmail.com>
2019-10-26 02:28:44 -0600

    Dimensión-Productos

--- backend/Models/Dimension.cs
+++ backend/Models/Dimension.cs
@@ -7,6 +7,8 @@ namespace backend.Models
     [Table("tb_dimension")]
     public class Dimension
     {
+        [Key]
+        public int id_dimension { get; set; }
         [Column(TypeName = "varchar(45)")]
         public string nombre_dimension { get; set; }
         public decimal valor { get; set; }
